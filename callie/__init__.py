10684d8fa295a28e72dde5a6fbf0ffab27a7961b
kevinam99 <kevinam99@gmail.com>
2020-05-17 23:25:37 +0530

    Initiated Flask project

---
+++ callie/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+# Initialize the app
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the views
+from callie import views
+
+# Load the config file
+app.config.from_object('config')
