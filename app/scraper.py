411d255e061424e0fc11d207d042245feb1ab993
kevinam99 <kevinam99@gmail.com>
2020-05-28 03:04:01 +0530

    Added Heroku path to Firefox

--- app/scraper.py
+++ app/scraper.py
@@ -17,7 +17,7 @@ class InstagramBot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.bot = webdriver.Firefox(capabilities=cap, firefox_options=options)
+        self.bot = webdriver.Firefox(capabilities=cap, firefox_options=options, executable_path = '../vendor/firefox/firefox')
 
 
     
