262a094bc6622ac5642c300901d693556317ab50
kevinam99 <kevinam99@gmail.com>
2020-05-28 22:31:52 +0530

    Using Chrome driver

--- app/scraper.py
+++ app/scraper.py
@@ -12,9 +12,9 @@ class InstagramBot:
     def use_chrome(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--no-sandbox")
         return webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     def use_firefox(self):
@@ -25,7 +25,7 @@ class InstagramBot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.bot = self.use_firefox()
+        self.bot = self.use_chrome()
 
 
     
