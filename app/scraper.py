a0190d3453790ec08636011c2dbf299bb884e837
kevinam99 <kevinam99@gmail.com>
2020-05-28 22:36:14 +0530

    Only headless option commented

--- app/scraper.py
+++ app/scraper.py
@@ -13,8 +13,8 @@ class InstagramBot:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-        # chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
         return webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     def use_firefox(self):
